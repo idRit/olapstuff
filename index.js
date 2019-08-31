b85e793096b7db631434e1de005839ce8f02a5e4
Ritwik Mukherjee <mritwik369@gmail.com>
2019-08-31 12:14:39 +0530

    yete

--- index.js
+++ index.js
@@ -3,17 +3,54 @@ const fetch = require('node-fetch');
 async function getAlltransactions() {
     let res = await (await fetch("https://raw.githubusercontent.com/idRit/olapstuff/master/transactions.json")).json();
     console.log(res);
-    let commitedTransactions = [];
-    res.reverse();
-    let firstTransactionName = res[0].name;
-    res.forEach((el, i) => {
-        if (i !== 0) {
-            
-            commitedTransactions.push(el.name);
-
+    let logArr = [];
+    let context = [];
+    let dataItemA = getRandomInt(1000);
+    let dataItemAHistory = [];
+    dataItemAHistory.push(dataItemA);
+    let newDataItem = dataItemA;
+    res.forEach(el => {
+        let log = "";
+        if (el.status === "start") {
+            log = "<" + el.name + ", " + "start>";
         }
+        else if (el.status === "commited") {
+            log = "<" + el.name + ", " + "commit>";
+        }
+        else if (el.operation.startsWith("read")) {
+            log = "<" + el.name + ", " + el.data + ", " + dataItemA + ", " + newDataItem + ">";
+        }
+        else if (el.status === "") {
+            if (el.operation.startsWith("add")) {
+                newDataItem += getNum(el.operation);
+                dataItemAHistory.push(newDataItem);
+                
+            } 
+            else {
+                newDataItem -= getNum(el.operation);
+                dataItemAHistory.push(newDataItem);
+            }
+            log = "<" + el.name + ", " + el.data + ", " + dataItemA + ", " + newDataItem + ">";
+        }
+        else if (el.status === "pc") {
+            log = "<" + el.name + ", " + "write>";
+        }
+        logArr.push(log);
     });
+    console.log(logArr);//.reverse());
+    // res.forEach((el, i) => {
+    //     if (el.name === elInit.name && el.operation === "commited") {
+    //         redo(el, context);
+    //     }    
+    // });
+}
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getNum(str) {
+    return parseInt(str.match(/(\d+)/));
 }
 
 getAlltransactions();
